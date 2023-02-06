b3981b2c1e8d1d8eb30dc8c90a51d560985e8d9c
Ali AYTEKİN <96261457+aytek55@users.noreply.github.com>
2023-02-06 23:14:28 +0300

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MernisService;
+
+namespace tckimlikdogrulama2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async void OnGet()
+        {
+            var client = new MernisService.KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            var response = await client.TCKimlikNoDogrulaAsync(Convert.ToInt64(12345678912), "Adınızıyazın", "Soyadınızıyazın", 1111);
+            var result = response.Body.TCKimlikNoDogrulaResult;
+
+        }
+    }
+}
